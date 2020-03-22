a513999cf22d3368ce44342a97542c5171bb469d
叶永明 <276680934@qq.com>
2020-03-22 17:06:22 +0800

    commit 登录获取用户信息相关代码

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 ## 码匠社区
 
 ## 资料
+[Spring 文档](https://spring.io/guides)   
+[Spring Web文档](https://spring.io/guides/gs/serving-web-content/)  
+[Github depoly key](http://developer.github.com/v3/guides/managing-depoly-keys/#depoly-keys)  
+[Bootstrap](https://v3.bootcss.com/getting-started/)  
+[Github OAuth](https://developer.github.com/apps/building-oauth-apps/creating-an-oauth-app/)  
 
 ## 工具
+[Git](https://git-scm.com/download)  
+[Visual Paradigm](https://www.visual-paradigm.com)
---
+++ src/main/java/life/majiang/community/controller/AuthorizeController.java
@@ -0,0 +1,32 @@
+package life.majiang.community.controller;
+
+import life.majiang.community.dto.AccessTokenDTO;
+import life.majiang.community.dto.GithubUser;
+import life.majiang.community.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AuthorizeController {
+
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name = "code") String code,
+                           @RequestParam(name = "state") String state){
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id("4b0605961d8b03821c16");
+        accessTokenDTO.setClient_secret("3209fb7af760b6b6b0670b57bcd23208568a140e");
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
+        accessTokenDTO.setState(state);
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user.getName());
+        return "index";
+    }
+
+}
