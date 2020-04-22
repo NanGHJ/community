2a8fc7b43e755e9497bbe2a2a76a921792184272
叶永明 <276680934@qq.com>
2020-04-22 23:34:14 +0800

    P44前60分钟

--- src/main/java/life/majiang/community/controller/CommentController.java
+++ src/main/java/life/majiang/community/controller/CommentController.java
@@ -1,7 +1,9 @@
 package life.majiang.community.controller;
 
 import life.majiang.community.dto.CommentCreateDTO;
+import life.majiang.community.dto.CommentDTO;
 import life.majiang.community.dto.ResultDTO;
+import life.majiang.community.enums.CommentTypeEnum;
 import life.majiang.community.exception.CustomizeErrorCode;
 import life.majiang.community.model.Comment;
 import life.majiang.community.model.User;
@@ -9,13 +11,11 @@ import life.majiang.community.service.CommentService;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 
 /*
@@ -28,15 +28,15 @@ public class CommentController {
     private CommentService commentService;
 
     @ResponseBody
-    @RequestMapping(value = "/comment",method = RequestMethod.POST)
+    @RequestMapping(value = "/comment", method = RequestMethod.POST)
     public Object post(@RequestBody CommentCreateDTO commentCreateDTO,
-                       HttpServletRequest request){
+                       HttpServletRequest request) {
         User user = (User) request.getSession().getAttribute("user");
-        if(user == null){
+        if (user == null) {
             return ResultDTO.errorOf(CustomizeErrorCode.NOT_LOGIN);
         }
 
-        if(commentCreateDTO == null || StringUtils.isBlank(commentCreateDTO.getContent())){
+        if (commentCreateDTO == null || StringUtils.isBlank(commentCreateDTO.getContent())) {
             return ResultDTO.errorOf(CustomizeErrorCode.COMMENT_IS_EMPTY);
         }
 
@@ -51,4 +51,15 @@ public class CommentController {
         commentService.insert(comment);
         return ResultDTO.okOf();
     }
+
+    /**
+     * 二级评论
+     */
+    @ResponseBody
+    @RequestMapping(value = "/comment/{id}", method = RequestMethod.GET)
+    public ResultDTO<List<CommentDTO>> comments(@PathVariable(name = "id") Long id) {
+        List<CommentDTO> commentDTOS = commentService.listByTargetId(id, CommentTypeEnum.COMMENT);
+        return ResultDTO.okOf(commentDTOS);
+    }
+
 }
--- src/main/resources/static/js/community.js
+++ src/main/resources/static/js/community.js
@@ -1,39 +1,77 @@
+/**
+ * 提交回复
+ */
 function post() {
     var questionId = $("#question_id").val();
     var content = $("#comment_content").val();
+    comment2target(questionId, 1, content);
 
-    if(!content){
+}
+
+function comment2target(targetId, type, content) {
+    if (!content) {
         alert("不能回复空内容！！！")
         return;
     }
 
     $.ajax({
-        type:"POST",
-        url:"/comment",
-        contentType:'application/json',
-        data:JSON.stringify({
-            "parentId":questionId,
-            "content":content,
-            "type":1
+        type: "POST",
+        url: "/comment",
+        contentType: 'application/json',
+        data: JSON.stringify({
+            "parentId": targetId,
+            "content": content,
+            "type": type
         }),
-        success:function (response) {
-            if(response.code == 200){
+        success: function (response) {
+            if (response.code == 200) {
                 window.location.reload();
-            }else {
-                if(response.code == 2003){
+            } else {
+                if (response.code == 2003) {
                     var isAccepted = confirm(response.message);
-                    if(isAccepted){
+                    if (isAccepted) {
                         window.open("https://github.com/login/oauth/authorize?client_id=4b0605961d8b03821c16&redirect_uri=http://localhost:8887/callback&scope=user&state=1");
-                        window.localStorage.setItem("closable",true);
+                        window.localStorage.setItem("closable", true);
                     }
-                }else{
+                } else {
                     alert(response.message);
                 }
             }
             console.log(response);
         },
-        dataType:"json"
+        dataType: "json"
     });
+}
 
+/**
+ * 提交二级回复
+ */
+function comment(e) {
+    var commentId = e.getAttribute("data-id");
 
+    var content = $("#input-" + commentId).val();
+    console.log(content);
+    comment2target(commentId, 2, content);
+}
+
+/**
+ * 展开二级评论
+ */
+function collapseComments(e) {
+    var id = e.getAttribute("data-id");
+    var comments = $("#comment-" + id);
+    //获取一下二级评论的展开状态
+    var collapse = e.getAttribute("data-collapse");
+    if (collapse) {
+        //折叠二级评论
+        comments.removeClass("in");
+        e.removeAttribute("data-collapse");
+        e.classList.remove("active");
+    } else {
+        //展开二级评论
+        comments.addClass("in");
+        //标记二级评论展开状态
+        e.setAttribute("data-collapse", "in");
+        e.classList.add("active");
+    }
 }
