a1b1cb2737c7f45aa4e92d0bde7cace5cbf250fb
叶永明 <276680934@qq.com>
2020-05-19 21:18:44 +0800

    P49前75分钟

--- src/main/java/life/majiang/community/enums/NotificationTypeEnum.java
+++ src/main/java/life/majiang/community/enums/NotificationTypeEnum.java
@@ -4,7 +4,7 @@ public enum NotificationTypeEnum {
 
     //设置回复type的类型
     REPLY_QUESTION(1,"回复了问题"),
-    REPLY_COMMENT(1,"回复了问题")
+    REPLY_COMMENT(2,"回复了评论")
     ;
     private int type;
     private String name;
@@ -20,5 +20,14 @@ public enum NotificationTypeEnum {
     NotificationTypeEnum(int status, String name) {
         this.type = status;
         this.name = name;
+    }
+
+    public static String nameOfType(int type){
+        for (NotificationTypeEnum notificationTypeEnum : NotificationTypeEnum.values()) {
+            if(notificationTypeEnum.getType() == type){
+                return notificationTypeEnum.getName();
+            }
+        }
+        return "";
     }
 }
--- src/main/java/life/majiang/community/service/CommentService.java
+++ src/main/java/life/majiang/community/service/CommentService.java
@@ -41,7 +41,7 @@ public class CommentService {
     private NotificationMapper notificationMapper;
 
     @Transactional
-    public void insert(Comment comment) {
+    public void insert(Comment comment, User commentator) {
         if (comment.getParentId() == null || comment.getParentId() == 0) {
             throw new CustomizeException(CustomizeErrorCode.TARGET_PARAM_NOT_FOUND);
         }
@@ -56,6 +56,13 @@ public class CommentService {
             if (dbComment == null) {
                 throw new CustomizeException(CustomizeErrorCode.COMMENT_NOT_FOUND);
             }
+
+            //回复问题
+            Question question = questionMapper.selectByPrimaryKey(dbComment.getParentId());
+            if (question == null) {
+                throw new CustomizeException(CustomizeErrorCode.QUESTION_NOT_FOUND);
+            }
+
             commentMapper.insert(comment);
 
             //增加评论数
@@ -65,7 +72,7 @@ public class CommentService {
             commentExtMapper.incCommentCount(parentComment);
 
             //创建通知
-            createNotify(comment, dbComment.getCommentator(), NotificationTypeEnum.REPLY_COMMENT);
+            createNotify(comment, dbComment.getCommentator(), commentator.getName(), question.getTitle(), NotificationTypeEnum.REPLY_COMMENT);
 
         } else {
             //回复问题
@@ -78,11 +85,11 @@ public class CommentService {
             questionExtMapper.incCommentCount(question);
 
             //创建通知
-            createNotify(comment,question.getCreator(), NotificationTypeEnum.REPLY_QUESTION);
+            createNotify(comment,question.getCreator(), commentator.getName(), question.getTitle(), NotificationTypeEnum.REPLY_QUESTION);
         }
     }
 
-    private void createNotify(Comment comment, Long receiver, NotificationTypeEnum notificationType) {
+    private void createNotify(Comment comment, Long receiver, String notifierName, String outerTitle, NotificationTypeEnum notificationType) {
         Notification notification = new Notification();
         notification.setGmtCreate(System.currentTimeMillis());
         notification.setType(notificationType.getType());
@@ -90,6 +97,8 @@ public class CommentService {
         notification.setNotifier(comment.getCommentator());
         notification.setStatus(NotificationStstusEnum.UNREAD.getStatus());
         notification.setReceiver(receiver);
+        notification.setNotifierName(notifierName);
+        notification.setOuterTitle(outerTitle);
         notificationMapper.insert(notification);
     }
 
